13cfacd2996ced3b7dcbe2e714f6c4dd09e96e04
traipe <jimenatraipe@gmail.com>
2024-05-30 13:11:02 -0400

    creacion de aplicacion y vinculacion de vistas

--- onlyflansPY/onlyflansPY/urls.py
+++ onlyflansPY/onlyflansPY/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from web.views import index, about, welcome
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('about/', about),
+    path('contact/', welcome),
 ]
+
+
+
+
