684080e6ae087a8096c5e51e1048df1b604a8d58
traipe <jimenatraipe@gmail.com>
2024-06-05 15:27:56 -0400

    Hito3, requerimientos 1 al 4, req 4 en subcarpeta para mejor orden

--- onlyflansPY/onlyflansPY/urls.py
+++ onlyflansPY/onlyflansPY/urls.py
@@ -16,15 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from web.views import index, about, welcome
+from web.views import index, about, welcome, success, contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('about/', about, name='about'),
     path('welcome/', welcome, name='welcome'),
+    path('contact/', contact, name='contact'),
+    path('success/', success, name='success'),
 ]
-
 
 
 
--- onlyflansPY/web/views.py
+++ onlyflansPY/web/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Flan
+from .forms import ContactFormModelForm
+from django.http import HttpResponseRedirect
+
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    flanes_publicos = Flan.objects.filter(is_private=False)
+    return render(request, 'index.html', {'flanes': flanes_publicos})
 
 def about(request):
     return render(request, 'about.html')
 
 def welcome(request):
-    return render(request, 'welcome.html')
+    flanes_privados = Flan.objects.filter(is_private=True)
+    return render(request, 'welcome.html', {'flanes': flanes_privados})
+
+# contacto 
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactFormModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/success/')
+    else:
+        form = ContactFormModelForm()
+    return render(request, 'contact.html', {'form': form})
+
+def success(request):
+    return render(request, 'success.html')
