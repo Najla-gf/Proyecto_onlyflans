dc27c587ec1572f0ae192430f98f56215f3d4b6b
Najla Gatica <najla.gatica@gmail.com>
2024-06-06 13:44:42 -0400

    Hito 3: modificación de formulario y estilo de página

--- onlyflansPY/web/forms.py
+++ onlyflansPY/web/forms.py
@@ -5,3 +5,13 @@ class ContactFormModelForm(forms.ModelForm):
     class Meta:
         model = ContactForm
         fields = ['customer_email', 'customer_name', 'message']
+        labels = {
+            'customer_email': 'Correo Electrónico',
+            'customer_name': 'Nombre',
+            'message': 'Mensaje',
+        }
+        widgets = {
+            'customer_email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'correo@ejemplo.com'}),
+            'customer_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre Completo'}),
+            'message': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Escribe tu mensaje aquí...', 'rows': 3}),
+        }
--- onlyflansPY/web/models.py
+++ onlyflansPY/web/models.py
@@ -2,12 +2,14 @@ from django.db import models
 import uuid
 
 class Flan(models.Model):
-    flan_uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    flan_uuid = models.UUIDField()
     name = models.CharField(max_length=64)
     description = models.TextField()
     image_url = models.URLField()
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField()
     is_private = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)  
 
     def __str__(self):
         return self.name
